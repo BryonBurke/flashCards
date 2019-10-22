34581143a1f8c688b97d96377e5d48b52bff0415
Student <example@example.com>
2019-10-22 16:13:43 -0700

    add funtionality

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,8 +1,21 @@
 $(document).ready(function() {
-  $(".card").click(function() {
+  $("#card1").click(function() {
+    $("#card1").find("p").removeClass();
+    $("#card1").addClass("word-and-definition");
+  });
+
+
+
+  $("#card2").click(function() {
     $("p").removeClass();
     $(".card").addClass("word-and-definition");
+  });
 
+
+
+  $("#card3").click(function() {
+    $("p").removeClass();
+    $(".card").addClass("word-and-definition");
+  });
 
   });
-});
